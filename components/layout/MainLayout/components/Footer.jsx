4a5b2b663f0a258debc343b4a08e8a51a5f2c419
Boris Belmar <borisbelmarm@gmail.com>
2022-05-30 10:30:46 -0400

    Made in Chile on Footer

--- components/layout/MainLayout/components/Footer.jsx
+++ components/layout/MainLayout/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer () {
   return (
     <footer className="flex justify-between px-16 py-4 bg-black text-white">
       <div className="flex-1 text-center">
-        Cuatro Cuatro Noventa
+        Made in Chile
       </div>
     </footer>
   )
