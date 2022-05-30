e9746fbbcbab763e6e63607ea5ab252925872d86
Boris Belmar <borisbelmarm@gmail.com>
2022-05-30 18:49:18 -0400

    Update home video

--- components/views/Home/components/VideoHome.jsx
+++ components/views/Home/components/VideoHome.jsx
@@ -7,7 +7,7 @@ export default function VideoHome () {
   return (
     <div ref={ref} className="w-full h-screen relative">
       <video className="w-full h-screen object-cover" autoPlay loop muted>
-        <source src="/assets/video-home.webm" type="video/webm" />
+        <source src="/assets/video-home2.webm" type="video/webm" />
       </video>
       <div className="absolute w-full h-full flex top-0 items-end justify-center left-0 text-white pb-8 bg-gray-900 bg-opacity-25 z-10">
         <motion.div
